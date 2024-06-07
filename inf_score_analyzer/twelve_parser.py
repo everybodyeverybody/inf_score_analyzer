d2cd62e644b15442c00c6fb39ec6f17853d583e7
John Scanlan <john.scanlan@gmail.com>
2024-06-07 00:26:16 -0400

    initial work to import community song ranks

--- inf_score_analyzer/twelve_parser.py
+++ inf_score_analyzer/twelve_parser.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+
+import csv
+import requests
+
+response = requests.get("https://iidx-sp12.github.io/songs.json")
+data = response.json()
+
+with open("checklist.csv", "wt") as writer:
+    csv_writer = csv.writer(writer)
+    header = ["name", "difficulty", "normal", "hard"]
+    csv_writer.writerow(header)
+    for entry in data:
+        row = [entry[key] for key in header]
+        csv_writer.writerow(row)
